2d2f64679f24e4f4999a50b4febe15f2a944d2e0
khadim Raath <khadim.raath@gmail.com>
2016-11-30 20:37:10 +0500

    for jobs

--- src/scrapers/main.java
+++ src/scrapers/main.java
@@ -15,7 +15,8 @@ import org.jsoup.nodes.Document;
 public class main {
 
     public static void main(String[] args) throws IOException {
-
+        
+        //////////////
         String tag = "for-sale";
 
         int pageFrom = 0;
@@ -33,129 +34,129 @@ public class main {
         pageFrom = pageTo;
         pageTo = pageFrom + increment;
         new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-        
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-        
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-
-        pageFrom = pageTo;
-        pageTo = pageFrom + increment;
-        new gumtree(tag, pageFrom, pageTo);
-        
-        
-        System.out.println(pageFrom);
-        System.out.println(pageTo);
-        // System.exit(1);
-
-        System.out.println("Done.");
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//        
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//        
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//
+//        pageFrom = pageTo;
+//        pageTo = pageFrom + increment;
+//        new gumtree(tag, pageFrom, pageTo);
+//        
+//        
+//        System.out.println(pageFrom);
+//        System.out.println(pageTo);
+//        // System.exit(1);
+//
+//        System.out.println("Done.");
     }
 
 }
